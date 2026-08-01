d2054d9c37ae987d6ba80e7ecd3801f5ee0c0c5d
Zaidu19 <zaidmohammad451@gmail.com>
2026-08-01 22:06:02 +0530

    Refactor payment workflow and improve payment lifecycle

--- src/payments/models.py
+++ src/payments/models.py
@@ -2,7 +2,7 @@ from uuid import UUID
 from datetime import datetime
 from decimal import Decimal
 from sqlalchemy.orm import Mapped,mapped_column,relationship
-from sqlalchemy import ForeignKey,Enum,func,DateTime,Numeric
+from sqlalchemy import ForeignKey,Enum,DateTime,Numeric
 
 from src.db.base import Base
 from src.common.mixin import UUIDPrimaryKeyMixin,TimestampMixin
@@ -13,10 +13,10 @@ if TYPE_CHECKING:
 class PaymentModel(UUIDPrimaryKeyMixin,TimestampMixin,Base):
     __tablename__ = "payments"
       
-    order_id:Mapped[UUID] = mapped_column(ForeignKey("orders.id"),nullable=False)
+    order_id:Mapped[UUID] = mapped_column(ForeignKey("orders.id"),nullable=False,unique=True)
     amount:Mapped[Decimal] = mapped_column(Numeric(12,2),nullable=False)
     payment_method:Mapped[PaymentMethod]=mapped_column(Enum(PaymentMethod),nullable=False)
     payment_status:Mapped[PaymentStatus] = mapped_column(Enum(PaymentStatus),default=PaymentStatus.pending,nullable=False)
-    paid_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),nullable=True)
+    paid_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),nullable=True)
 
     order:Mapped["OrderModel"]=relationship(back_populates="payments")
--- src/payments/service.py
+++ src/payments/service.py
@@ -11,42 +11,68 @@ from src.payments.models import PaymentModel
 from src.orders.models import OrderModel
 from src.common.enum import OrderStatus
 
+VALID_PAYMENT_TRANSITIONS ={
+    PaymentStatus.pending:[
+        PaymentStatus.success,
+        PaymentStatus.failed
+    ],
+    PaymentStatus.failed:[
+        PaymentStatus.pending,
+    ],
+    PaymentStatus.success:[],
+    PaymentStatus.refunded:[]
+}
+
 
 def create_payment(payload:PaymentCreateSchema,db:Session)->PaymentModel:
-    order = db.get(OrderModel,payload.order_id)
-    if not order:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Order not found")
+    try:
+        order = db.get(OrderModel,payload.order_id)
+        if not order:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Order not found")
 
-    if order.status == OrderStatus.cancelled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Cannot create payment for cancelled order")
+        if order.status == OrderStatus.cancelled:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Cannot create payment for cancelled order")
 
-    existing_payment = db.scalars(select(PaymentModel).where(PaymentModel.order_id == payload.order_id)
-                                  .order_by(PaymentModel.created_at.desc())).first()
-    if existing_payment:
-
-        if existing_payment.payment_status == PaymentStatus.pending:
-            return existing_payment
-        
-        if existing_payment.payment_status == PaymentStatus.success:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already completed")
-
-        if existing_payment.payment_status == PaymentStatus.refunded:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already refunded")
+        existing_payment = db.scalars(select(PaymentModel).where(PaymentModel.order_id == payload.order_id)
+                                    .order_by(PaymentModel.created_at.desc())).first()
+        if existing_payment:
 
 
-    new_payment = PaymentModel(
-        order_id = order.id,
-        amount = order.grand_total,
-        payment_method = payload.payment_method,
-        payment_status = PaymentStatus.pending,
-        paid_at =None,
-    )
+            if existing_payment.payment_status == PaymentStatus.pending:
+               return existing_payment
+            
+            elif existing_payment.payment_status == PaymentStatus.success:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already completed")
 
-    db.add(new_payment)
-    db.commit()
-    db.refresh(new_payment)
+            elif existing_payment.payment_status == PaymentStatus.refunded:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already refunded")
+            elif existing_payment.payment_status == PaymentStatus.failed:
+               existing_payment.payment_status = PaymentStatus.pending
+               existing_payment.payment_method = payload.payment_method
+               existing_payment.paid_at = None
 
-    return new_payment
+               db.commit()
+               db.refresh(existing_payment)
+
+               return existing_payment
+
+
+        new_payment = PaymentModel(
+            order_id = order.id,
+            amount = order.grand_total,
+            payment_method = payload.payment_method,
+            payment_status = PaymentStatus.pending,
+            paid_at =None,
+        )
+
+        db.add(new_payment)
+        db.commit()
+        db.refresh(new_payment)
+
+        return new_payment
+    except Exception:
+        db.rollback()
+        raise
 
 def update_payment_status(payment_id:UUID,payload:PaymentUpdateSchema,db:Session)->PaymentModel:
     payment = db.get(PaymentModel,payment_id)
@@ -54,22 +80,26 @@ def update_payment_status(payment_id:UUID,payload:PaymentUpdateSchema,db:Session
     if not payment:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Payment not found")
 
-    if payment.payment_status == PaymentStatus.success:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already completed")
+    if payload.payment_status == payment.payment_status:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already has this status.")
 
-    if payment.payment_status == payload.payment_status:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Payment already has this status")
+    if payload.payment_status not in VALID_PAYMENT_TRANSITIONS[payment.payment_status]:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Cannot change payment from {payment.payment_status.value} to {payload.payment_status.value}")
 
-    payment.payment_status = payload.payment_status
-
+    
     if payload.payment_status == PaymentStatus.success:
         order = db.get(OrderModel,payment.order_id)
 
         if order.status == OrderStatus.cancelled:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Cannot complete payment for cancelled order")
 
+    payment.payment_status = payload.payment_status 
+
+    if payload.payment_status == PaymentStatus.success:
         order.status = OrderStatus.paid
         payment.paid_at = datetime.now(timezone.utc)
+       
 
     elif payload.payment_status == PaymentStatus.failed:
         payment.paid_at = None
