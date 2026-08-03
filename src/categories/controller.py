e18e7fd098eeae088c75490272502153d7136e6e
Zaidu19 <zaidmohammad451@gmail.com>
2026-08-03 13:02:41 +0530

    feat:secure categories-module

--- src/categories/controller.py
+++ src/categories/controller.py
@@ -3,6 +3,12 @@ from uuid import UUID
 from fastapi import APIRouter,status,Depends
 from sqlalchemy.orm import Session
 
+from src.users.models import UserModel
+from src.auth.service import(
+    get_current_admin,
+    get_current_user,
+   
+)
 from src.categories.dtos import(
     CategoryCreateSchema,
     CategoryUpdateSchema,
@@ -21,21 +27,21 @@ from src.db.database import get_db
 router = APIRouter(prefix="/categories",tags=["Categories"])
 
 @router.post("/",response_model=CategoryResponseSchema,status_code=status.HTTP_201_CREATED)
-def create_category_endpoint(payload:CategoryCreateSchema,db:Session=Depends(get_db)):
+def create_category_endpoint(payload:CategoryCreateSchema,db:Session=Depends(get_db),_:UserModel=Depends(get_current_admin)):
     return create_category(payload,db)
 
 @router.get("/",response_model=list[CategoryResponseSchema],status_code=status.HTTP_200_OK)
-def get_all_categories_endpont(db:Session=Depends(get_db)):
+def get_all_categories_endpoint(db:Session=Depends(get_db),_:UserModel=Depends(get_current_user)):
     return get_all_categories(db)
 
 @router.get("/{category_id}",response_model=CategoryResponseSchema,status_code=status.HTTP_200_OK)
-def get_category_by_id_endpoint(category_id:UUID,db:Session=Depends(get_db)):
+def get_category_by_id_endpoint(category_id:UUID,db:Session=Depends(get_db),_:UserModel=Depends(get_current_user)):
     return get_category_by_id(category_id,db)
 
 @router.put("/{category_id}",response_model=CategoryResponseSchema,status_code=status.HTTP_200_OK)
-def update_category_endpoint(category_id:UUID,payload:CategoryUpdateSchema,db:Session=Depends(get_db)):
+def update_category_endpoint(category_id:UUID,payload:CategoryUpdateSchema,db:Session=Depends(get_db),_:UserModel=Depends(get_current_admin)):
     return update_category(category_id,payload,db)
 
 @router.delete("/{category_id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_category_endpoint(category_id:UUID,db:Session=Depends(get_db)):
+def delete_category_endpoint(category_id:UUID,db:Session=Depends(get_db),_:UserModel=Depends(get_current_admin)):
     return delete_category(category_id,db)
